3df6f80d4653fe40c873b177513c4c8bc8e780a0
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2020-07-09 21:30:07 +0900

    fix typo

--- segmantation.py
+++ segmantation.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     res1 = kmeans.predict(image1_data)
 
     # 二次元配列に戻す
-    image1_result = ImageUtils.reshape([300, 300])
+    image1_result = ImageUtils.reshape(res1, [300, 300])
 
     # matplotlibで描画
     y = range(300)
